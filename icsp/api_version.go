457ebd453f8483b9659a51fd062e5aee4675b105
Miguel Quintero <miguel.quintero@hp.com>
2015-09-08 17:16:31 -0400

    version api call

---
+++ icsp/api_version.go
@@ -0,0 +1,35 @@
+package ov
+
+import (
+	"encoding/json"
+	"github.com/docker/machine/log"
+	"github.com/docker/machine/drivers/oneview/rest"
+)
+
+// APIVersion struct
+type APIVersion struct {
+	CurrentVersion   int    `json:"currentVersion,omitempty"`
+	MinimumVersion   int    `json:"minimumVersion,omitempty"`
+}
+
+// GetAPIVersion - returns the api version for OneView server
+// returns structure APIVersion
+func  (c *OVClient) GetAPIVersion() (APIVersion, error) {
+	var (
+		uri    = "/rest/version"
+	  apiversion APIVersion
+	)
+
+	//c.AuthHeaders := map[string]interface{}{"auth": []interface{}{auth}}
+	c.SetAuthHeaderOptions( c.GetAuthHeaderMap() )
+	data, err := c.RestAPICall(rest.GET, uri , nil)
+	if err != nil {
+		return apiversion, err
+	}
+
+	log.Debugf("GetAPIVersion %s", data)
+	if err := json.Unmarshal([]byte(data), &apiversion); err != nil {
+		return apiversion, err
+	}
+  return apiversion, err
+}
---
+++ icsp/api_version_test.go
@@ -0,0 +1,33 @@
+package icsp
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Test GetAPIVersion
+func TestGetAPIVersion(t *testing.T) {
+	var (
+		c *OVClient
+	)
+	if os.Getenv("ICSP_TEST_ACCEPTANCE") == "true" {
+		c = getTestDriverA()
+		if c == nil {
+			t.Fatalf("Failed to execute getTestDriver() ")
+		}
+		data, err := c.GetAPIVersion()
+		// fmt.Printf("after GetAPIVersion: %s -> (err) %s", data.CurrentVersion, err)
+		// assert.Error(t,err, fmt.Sprintf("Error caught as expected: %s",err))
+		assert.NoError(t, err, "GetAPIVersion threw error -> %s", err)
+		assert.Equal(t, 120, data.CurrentVersion)
+		assert.Equal(t, 1, data.MinimumVersion)
+	} else {
+		c = getTestDriverU()
+		data, err := c.GetAPIVersion()
+		assert.Error(t, err, fmt.Sprintf("ALL ok, no error, caught as expected: %s,%+v\n", err, data))
+	}
+
+}
