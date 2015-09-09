82c2432736ae0c2c293943e80714563503e01367
Miguel Quintero <miguel.quintero@hp.com>
2015-09-09 01:24:05 -0400

    add isempty helper...

--- rest/helpers.go
+++ rest/helpers.go
@@ -2,10 +2,18 @@ package rest
 
 import "strings"
 
-// Helpers
-func (c *Client) Sanatize(s string) (string) {
+// Sanatize ...
+func (c *Client) Sanatize(s string) string {
 	if strings.LastIndex(s, "/") > 0 {
 		s = strings.Trim(s, "/")
 	}
 	return s
+}
+
+// IsEmpty ...
+func IsEmpty(s string) bool {
+	if s == null || len(strings.TrimSpace(s)) == 0 {
+		return true
+	}
+	return false
 }
