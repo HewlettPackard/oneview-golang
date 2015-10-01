7fe90a25bf30e313127d0cd56604cc042d1550c7
Edward Raigosa <wenlock@hp.com>
2015-09-30 17:11:11 -0700

    add test case for apply deployment plan
    Signed-off-by: Edward Raigosa <wenlock@hp.com>

--- icsp/deploymentjobs_test.go
+++ icsp/deploymentjobs_test.go
@@ -2,6 +2,7 @@ package icsp
 
 import (
 	"encoding/json"
+	"os"
 	"testing"
 
 	"github.com/docker/machine/log"
@@ -66,3 +67,44 @@ func TestOSDNicDataV2(t *testing.T) {
 }
 
 // TODO: ApplyDeploymentJobs
+// TestSaveServer implement save server
+func TestApplyDeploymentJobs(t *testing.T) {
+	var (
+		d *ICSPTest
+		c *ICSPClient
+	)
+	if os.Getenv("ICSP_TEST_ACCEPTANCE") == "true" {
+		log.Debug("implements acceptance test for ApplyDeploymentJobs")
+		d, c = getTestDriverA()
+		if c == nil {
+			t.Fatalf("Failed to execute getTestDriver() ")
+		}
+		// get a Server
+		osBuildPlan := d.Tc.GetTestData(d.Env, "OSBuildPlan").(string)
+		serialNumber := d.Tc.GetTestData(d.Env, "FreeBladeSerialNumber").(string)
+		s, err := c.GetServerBySerialNumber(serialNumber)
+		assert.NoError(t, err, "GetServerBySerialNumber threw error -> %s, %+v\n", err, s)
+		// set a custom attribute
+		s.SetCustomAttribute("docker_user", "server", "docker")
+		// use test keys like from https://github.com/mitchellh/vagrant/tree/master/keys
+		// private key from https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant
+		s.SetCustomAttribute("public_key", "server", "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ==")
+
+		// save a server
+		news, err := c.SaveServer(s)
+		assert.NoError(t, err, "SaveServer threw error -> %s, %+v\n", err, news)
+		assert.Equal(t, s.UUID, news.UUID, "Should return a server with the same UUID")
+
+		// verify that the server attribute was saved by getting the server again and checking the value
+		_, testValue2 := s.GetValueItem("docker_user", "server")
+		assert.Equal(t, "docker", testValue2.Value, "Should return the saved custom attribute")
+
+		err = c.ApplyDeploymentJobs(osBuildPlan, s)
+		assert.NoError(t, err, "ApplyDeploymentJobs threw error -> %s, %+v\n", err, news)
+	} else {
+		var s Server
+		log.Debug("implements unit test for ApplyDeploymentJobs")
+		err := c.ApplyDeploymentJobs("testbuildplan", s)
+		assert.Error(t, err, "ApplyDeploymentJobs threw error -> %s, %+v\n", err, s)
+	}
+}
