702245463b1fea8e3d8702519529e9977a7f6b96
Edward Raigosa <edward.raigosa@hpe.com>
2015-10-13 22:45:46 -0700

    fixup test cases, workaround
    Signed-off-by: Edward Raigosa <edward.raigosa@hpe.com>

--- liboneview/version_test.go
+++ liboneview/version_test.go
@@ -1,11 +1,10 @@
 package liboneview
 
 import (
-	"os"
 	"testing"
 
-	"github.com/docker/machine/drivers/oneview/icsp"
-	"github.com/docker/machine/drivers/oneview/ov"
+	// "github.com/docker/machine/drivers/oneview/icsp"
+	// "github.com/docker/machine/drivers/oneview/ov"
 	"github.com/docker/machine/log"
 	"github.com/stretchr/testify/assert"
 )
@@ -31,61 +30,70 @@ func TestCalculateVersion(t *testing.T) {
 
 // Test GetAPIVersion
 func TestCheckVersion(t *testing.T) {
-	var (
-		ClientICSP *icsp.ICSPClient
-		ClientOV   *ov.OVClient
-	)
-	if os.Getenv("ONEVIEW_TEST_ACCEPTANCE") == "true" {
+	// TODO: removing refrences to icsp/ov libs because we get this error:
+	// 	# github.com/docker/machine/drivers/oneview/liboneview
+	// import cycle not allowed in test
+	// package github.com/docker/machine/drivers/oneview/liboneview (test)
+	// 	imports github.com/docker/machine/drivers/oneview/ov
+	// 	imports github.com/docker/machine/drivers/oneview/liboneview
+	//
+	// FAIL	github.com/docker/machine/drivers/oneview/liboneview [setup failed]
 
-		/*Client := &icsp.ICSPClient{
-			rest.Client{
-				User:     os.Getenv("ONEVIEW_ICSP_USER"),
-				Password: os.Getenv("ONEVIEW_ICSP_PASSWORD"),
-				Domain:   os.Getenv("ONEVIEW_ICSP_DOMAIN"),
-				Endpoint: os.Getenv("ONEVIEW_ICSP_ENDPOINT"),
-				// ConfigDir:
-				SSLVerify:  false,
-				APIVersion: 108,
-				APIKey:     "none",
-			},
-		}*/
-		log.Debugf("%+v", os.Getenv("ONEVIEW_ICSP_USER"))
-		log.Debugf("%+v", os.Getenv("ONEVIEW_ICSP_PASSWORD"))
-		log.Debugf("%+v", os.Getenv("ONEVIEW_ICSP_ENDPOINT"))
-		c := ClientICSP.NewICSPClient(
-			os.Getenv("ONEVIEW_ICSP_USER"),
-			os.Getenv("ONEVIEW_ICSP_PASSWORD"),
-			os.Getenv("ONEVIEW_ICSP_DOMAIN"),
-			os.Getenv("ONEVIEW_ICSP_ENDPOINT"),
-			false,
-			108)
-		icspVer, _ := c.GetAPIVersion()
-		log.Debugf("%d", icspVer.CurrentVersion)
-
-		ovc := ClientOV.NewOVClient(
-			os.Getenv("ONEVIEW__OV_USER"),
-			os.Getenv("ONEVIEW_OV_PASSWORD"),
-			os.Getenv("ONEVIEW_OV_DOMAIN"),
-			os.Getenv("ONEVIEW_OV_ENDPOINT"),
-			false,
-			120)
-		ovVer, _ := ovc.GetAPIVersion()
-		log.Debugf("%d", ovVer.CurrentVersion)
-		verCombo := ovVer.CurrentVersion + icspVer.CurrentVersion
-		assert.True(t, IsVersionValid(verCombo))
-		// fmt.Printf("after GetAPIVersion: %s -> (err) %s", data.CurrentVersion, err)
-		// assert.Error(t,err, fmt.Sprintf("Error caught as expected: %s",err))
-		//assert.NoError(t, err, "GetAPIVersion threw error -> %s", err)
-		//assert.Equal(t, true, d.Tc.EqualFaceI(d.Tc.GetExpectsData(d.Env, "CurrentVersion"), data.CurrentVersion))
-		//assert.Equal(t, true, d.Tc.EqualFaceI(d.Tc.GetExpectsData(d.Env, "MinimumVersion"), data.MinimumVersion))
-
-	} else {
-		// quickie unit test
-		isValid := IsVersionValid(400)
-		assert.False(t, isValid)
-		// ov,120 + icsp,108
-		isValid = IsVersionValid(228)
-		assert.True(t, isValid)
-	}
+	// var (
+	// 	ClientICSP *icsp.ICSPClient
+	// 	ClientOV   *ov.OVClient
+	// )
+	// if os.Getenv("ONEVIEW_TEST_ACCEPTANCE") == "true" {
+	//
+	// 	/*Client := &icsp.ICSPClient{
+	// 		rest.Client{
+	// 			User:     os.Getenv("ONEVIEW_ICSP_USER"),
+	// 			Password: os.Getenv("ONEVIEW_ICSP_PASSWORD"),
+	// 			Domain:   os.Getenv("ONEVIEW_ICSP_DOMAIN"),
+	// 			Endpoint: os.Getenv("ONEVIEW_ICSP_ENDPOINT"),
+	// 			// ConfigDir:
+	// 			SSLVerify:  false,
+	// 			APIVersion: 108,
+	// 			APIKey:     "none",
+	// 		},
+	// 	}*/
+	// 	log.Debugf("%+v", os.Getenv("ONEVIEW_ICSP_USER"))
+	// 	log.Debugf("%+v", os.Getenv("ONEVIEW_ICSP_PASSWORD"))
+	// 	log.Debugf("%+v", os.Getenv("ONEVIEW_ICSP_ENDPOINT"))
+	// 	c := ClientICSP.NewICSPClient(
+	// 		os.Getenv("ONEVIEW_ICSP_USER"),
+	// 		os.Getenv("ONEVIEW_ICSP_PASSWORD"),
+	// 		os.Getenv("ONEVIEW_ICSP_DOMAIN"),
+	// 		os.Getenv("ONEVIEW_ICSP_ENDPOINT"),
+	// 		false,
+	// 		108)
+	// 	icspVer, _ := c.GetAPIVersion()
+	// 	log.Debugf("%d", icspVer.CurrentVersion)
+	//
+	// 	ovc := ClientOV.NewOVClient(
+	// 		os.Getenv("ONEVIEW__OV_USER"),
+	// 		os.Getenv("ONEVIEW_OV_PASSWORD"),
+	// 		os.Getenv("ONEVIEW_OV_DOMAIN"),
+	// 		os.Getenv("ONEVIEW_OV_ENDPOINT"),
+	// 		false,
+	// 		120)
+	// 	ovVer, _ := ovc.GetAPIVersion()
+	// 	log.Debugf("%d", ovVer.CurrentVersion)
+	// 	verCombo := ovVer.CurrentVersion + icspVer.CurrentVersion
+	// 	assert.True(t, IsVersionValid(verCombo))
+	// 	// fmt.Printf("after GetAPIVersion: %s -> (err) %s", data.CurrentVersion, err)
+	// 	// assert.Error(t,err, fmt.Sprintf("Error caught as expected: %s",err))
+	// 	//assert.NoError(t, err, "GetAPIVersion threw error -> %s", err)
+	// 	//assert.Equal(t, true, d.Tc.EqualFaceI(d.Tc.GetExpectsData(d.Env, "CurrentVersion"), data.CurrentVersion))
+	// 	//assert.Equal(t, true, d.Tc.EqualFaceI(d.Tc.GetExpectsData(d.Env, "MinimumVersion"), data.MinimumVersion))
+	//
+	// } else {
+	// quickie unit test
+	isValid := IsVersionValid(400)
+	assert.False(t, isValid)
+	// ov,120 + icsp,108
+	isValid = IsVersionValid(228)
+	assert.True(t, isValid)
+	// }
 
 }
