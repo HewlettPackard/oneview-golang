50ea509cf669e3a8bb1c5be52ad092b0cd5a4485
Miguel Quintero <miguel.quintero@hpe.com>
2015-10-23 16:35:27 -0400

    adding lic header and golang codestyle compliance
    Signed-off-by: Miguel Quintero <miguel.quintero@hpe.com>

--- oneview.go
+++ oneview.go
@@ -1,3 +1,21 @@
+/*
+
+(c) Copyright [2015] Hewlett Packard Enterprise Development LP
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package oneview provides docker-machine lifecycle calls to the OneView and ICSP REST APIs(TODO: this needs better wording..)
 package oneview
 
 import (
@@ -413,17 +431,17 @@ func (d *Driver) GetState() (state.State, error) {
 	if err := d.getBlade(); err != nil {
 		return state.Error, err
 	}
-	if icsp.PROVISIONING.Equal(d.Server.OpswLifecycle) {
+	if icsp.Provisioning.Equal(d.Server.OpswLifecycle) {
 		return state.Starting, nil
 	}
-	if icsp.UNPROVISIONED.Equal(d.Server.OpswLifecycle) ||
-		icsp.PRE_UNPROVISIONED.Equal(d.Server.OpswLifecycle) {
+	if icsp.Unprovisioned.Equal(d.Server.OpswLifecycle) ||
+		icsp.PreUnProvisioned.Equal(d.Server.OpswLifecycle) {
 		return state.Stopping, nil
 	}
-	if icsp.DEACTIVATED.Equal(d.Server.OpswLifecycle) {
+	if icsp.Deactivated.Equal(d.Server.OpswLifecycle) {
 		return state.Stopped, nil
 	}
-	if icsp.PROVISION_FAILED.Equal(d.Server.OpswLifecycle) {
+	if icsp.ProvisionedFailed.Equal(d.Server.OpswLifecycle) {
 		return state.Error, nil
 	}
 	// use power state to determine status
