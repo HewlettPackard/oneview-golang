b27b87d46961474bd182568c7e295db56b4b32da
Patrick D'appollonio <dappollonio@hpe.com>
2016-11-21 08:59:03 -0700

    Avoid recompiling regexp over and over. Type switch.
    Signed-off-by: Patrick D'appollonio <dappollonio@hpe.com>

--- utils/stringy.go
+++ utils/stringy.go
@@ -3,11 +3,14 @@ package utils
 import "regexp"
 
 // some general string function helpers
+var (
+	reRemoveJSON = regexp.MustCompile("(.*)({.*}).*")
+	reGetJSON    = regexp.MustCompile("(.*)({.*}).*")
+)
 
 // StringRemoveJSON - remove a json string from regular strings
 func StringRemoveJSON(s string) string {
-	r := regexp.MustCompile("(.*)({.*}).*")
-	a := r.FindStringSubmatch(s)
+	a := reRemoveJSON.FindStringSubmatch(s)
 	if len(a) > 2 {
 		return StringRemoveJSON(a[1]) // keep trying to remove json till there is no more left
 	}
@@ -17,8 +20,7 @@ func StringRemoveJSON(s string) string {
 // StringGetJSON - just get the JSON from the string
 //                 should only find the first json
 func StringGetJSON(s string) string {
-	r := regexp.MustCompile("(.*)({.*}).*")
-	a := r.FindStringSubmatch(s)
+	a := reGetJSON.FindStringSubmatch(s)
 	if len(a) > 2 {
 		return a[2]
 	}
