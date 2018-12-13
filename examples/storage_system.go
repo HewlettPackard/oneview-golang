db944a347d295eccdca516cb71b2663e6867d152
madhav-bharadwaj <madhavb@hpe.com>
2018-12-13 14:03:15 +0530

    added post endpoint
    Signed-off-by: madhav-bharadwaj <madhavb@hpe.com>

--- examples/storage_system.go
+++ examples/storage_system.go
@@ -11,7 +11,7 @@ func main() {
 
 	var (
 		ClientOV       *ov.OVClient
-//		new_name    = "TestSystem"
+//		new_system    = "TestSystem"
 		name_to_update = "ThreePAR-1"
 	)
 
@@ -24,18 +24,16 @@ func main() {
 		500,
 		"*")
 
-	// Create storage system with name <new_name>
-/*	properties := &ov.Properties{new_name, utils.NewNstring("/rest/storage-pools/AAA05D5E-BDB5-4FBB-8E65-A8D400A6A8AF"), 107374741824, "Thin"}
-
-	storageSystem := ov.StorageSystemV3{TemplateURI: utils.NewNstring("/rest/storage-system-templates/c93ef008-d8f0-40a5-b2d1-a8d400a6a8b7"), Properties: properties, IsPermanent: false}
+	// Create storage system with name <new_system>
+	storageSystem := ov.StorageSystemV4{Hostname: "172.18.11.11", Username:"dcs", Password:"dcs", Family: "StoreServ"}
 
 	err := ovc.CreateStorageSystem(storageSystem)
 	if err != nil {
 		fmt.Println("Could not create the system", err)
 	}
-
+/*
 	// Update the given storage system
-	update_system, _ := ovc.GetStorageSystemByName(new_name)
+	update_system, _ := ovc.GetStorageSystemByName(new_system)
 
 	updated_storage_system := ov.StorageSystemV3{
 		ProvisioningTypeForUpdate: update_system.ProvisioningTypeForUpdate,
