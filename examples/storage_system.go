4e1a67b09dce96d859146b3a9ed8a4f1a13a61eb
madhav-bharadwaj <madhavb@hpe.com>
2018-12-14 11:46:24 +0530

    Adding example for update
    Signed-off-by: madhav-bharadwaj <madhavb@hpe.com>

--- examples/storage_system.go
+++ examples/storage_system.go
@@ -3,16 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/HewlettPackard/oneview-golang/ov"
-//	"github.com/HewlettPackard/oneview-golang/utils"
 	"os"
 )
 
 func main() {
 
 	var (
-		ClientOV       *ov.OVClient
-//		new_system    = "TestSystem"
-		name_to_update = "ThreePAR-1"
+		ClientOV *ov.OVClient
+		//		new_system    = "TestSystem"
+		name_to_update  = "ThreePAR-1"
+		name_to_update1 = "ThreePAR-2"
 	)
 
 	ovc := ClientOV.NewOVClient(
@@ -25,33 +25,34 @@ func main() {
 		"*")
 
 	// Create storage system with name <new_system>
-	storageSystem := ov.StorageSystemV4{Hostname: "172.18.11.11", Username:"dcs", Password:"dcs", Family: "StoreServ"}
+	storageSystem := ov.StorageSystemV4{Hostname: "172.18.11.11", Username: "dcs", Password: "dcs", Family: "StoreServ"}
 
 	err := ovc.CreateStorageSystem(storageSystem)
 	if err != nil {
 		fmt.Println("Could not create the system", err)
 	}
-/*
-	// Update the given storage system
-	update_system, _ := ovc.GetStorageSystemByName(new_system)
 
-	updated_storage_system := ov.StorageSystemV3{
-		ProvisioningTypeForUpdate: update_system.ProvisioningTypeForUpdate,
-		IsPermanent:               update_system.IsPermanent,
-		IsShareable:               update_system.IsShareable,
-		Name:                      name_to_update,
-		ProvisionedCapacity:       "107374182400",
-		DeviceSpecificAttributes:  update_system.DeviceSpecificAttributes,
-		URI:                       update_system.URI,
-		ETAG:                      update_system.ETAG,
-		Description:               "empty",
+	// Update the given storage system
+	update_system, _ := ovc.GetStorageSystemByName(name_to_update)
+
+	DeviceSpecificAttributesForUpdate := update_system.StorageSystemDeviceSpecificAttributes
+	DeviceSpecificAttributesForUpdate.ManagedDomain = "TestDomain"
+
+	updated_storage_system := ov.StorageSystemV4{
+		Name: name_to_update1,
+		StorageSystemDeviceSpecificAttributes: DeviceSpecificAttributesForUpdate,
+		URI:         update_system.URI,
+		ETAG:        update_system.ETAG,
+		Description: "empty",
+		Credentials: update_system.Credentials,
+		Hostname:    update_system.Hostname,
+		Ports:       update_system.Ports,
 	}
 
 	err = ovc.UpdateStorageSystem(updated_storage_system)
 	if err != nil {
 		fmt.Println("Could not update the system", err)
 	}
-*/
 
 	// Get All the systems present
 	fmt.Println("\nGetting all the storage systems present in the system: \n")
