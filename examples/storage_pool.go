e4b3f677243980476f1189f1efaefe7595daebee
madhav-bharadwaj <madhavb@hpe.com>
2019-01-08 18:01:07 +0530

    initial lib and example file
    Signed-off-by: madhav-bharadwaj <madhavb@hpe.com>

---
+++ examples/storage_pool.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/HewlettPackard/oneview-golang/ov"
+//	"github.com/HewlettPackard/oneview-golang/utils"
+	"os"
+)
+
+func main() {
+
+	var (
+		ClientOV       *ov.OVClient
+		storage_pool = "CPG-SSD"
+	)
+
+	ovc := ClientOV.NewOVClient(
+		os.Getenv("ONEVIEW_OV_USER"),
+		os.Getenv("ONEVIEW_OV_PASSWORD"),
+		os.Getenv("ONEVIEW_OV_DOMAIN"),
+		os.Getenv("ONEVIEW_OV_ENDPOINT"),
+		false,
+		600,
+		"*")
+
+	//Get storage pool by name to update
+	update_pool, _ := ovc.GetStoragePoolByName(storage_pool)
+	fmt.Println(update_pool.IsManaged)
+	// Update the given storage pool
+	update_pool.IsManaged = false
+	fmt.Println(update_pool.IsManaged)
+
+	err := ovc.UpdateStoragePool(update_pool)
+	if err != nil {
+		fmt.Println("Could not update the pool", err)
+	}
+
+
+	// Get All the pools present
+	fmt.Println("\nGetting all the storage pools present in the system: \n")
+	sort := "name:desc"
+	pool_list, err := ovc.GetStoragePools("", sort)
+	if err != nil {
+		fmt.Println("Error Getting the storage pools ", err)
+	}
+	for i := 0; i < len(pool_list.Members); i++ {
+		fmt.Println(pool_list.Members[i].Name)
+	}
+}
