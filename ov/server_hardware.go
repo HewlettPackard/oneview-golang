f1fd48c387b2793b6c24dcd82fbdef00e0fff46f
supreethpy <s.supreeth@gmail.com>
2019-01-23 06:41:24 -0500

    changes done in endpoint-suppport file
    Signed-off-by: supreethpy <s.supreeth@gmail.com>

--- ov/server_hardware.go
+++ ov/server_hardware.go
@@ -21,9 +21,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	//"net/url"
 	"strings"
-
 	"github.com/HewlettPackard/oneview-golang/rest"
 	"github.com/HewlettPackard/oneview-golang/utils"
 	"github.com/docker/machine/libmachine/log"
