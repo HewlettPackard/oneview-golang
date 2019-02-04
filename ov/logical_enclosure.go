c631019f9e3bf1defeab5b5b0f28e4231ed0f6d4
sowmyahsg <sowmyahsg@gmail.com>
2019-02-04 11:44:55 +0000

    fixed code
    Signed-off-by: sowmyahsg <sowmyahsg@gmail.com>

--- ov/logical_enclosure.go
+++ ov/logical_enclosure.go
@@ -50,10 +50,14 @@ type DeploymentModeSettings struct {
 	DeploymentNetworkUri   utils.Nstring 	`json:"deploymentNetworkUri,omitempty"` 	//"deploymentNetworkUri":"/rest/ethernet-networks/e2f0031b-52bd-4223-9ac1-d91cb519d548"
 }
 
+type LE_OsDeploymentSettings struct {
+	DeploymentModeSettings *DeploymentModeSettings `json:"deploymentModeSettings,omitempty"`
+	ManageOSDeployment bool `json:"manageOSDeployment,omitempty"`
+}
+
 type DeploymentManagerSettings struct {
 	DeploymentClusterUri  utils.Nstring          `json:"deploymentClusterUri,omitempty"`	//"deploymentClusterUri":""
-	OsDeploymentSettings *DeploymentModeSettings `json:"deploymentModeSettings,omitempty"` //"deploymentModeSettings":""
-	ManageOSDeployment    bool                   `json:"manageOSDeployment,omitempty"`     //"manageOSDeployment":true
+	OsDeploymentSettings *LE_OsDeploymentSettings `json:"osDeploymentSettings,omitempty"` //"OsdeploymentSettings":""
 }
 
 type Enclosures struct {
