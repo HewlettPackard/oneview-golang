eea9b15064b7e3c018f7623aa394f258c701309f
sowmyahsg <sowmyahsg@gmail.com>
2019-02-06 10:26:28 +0000

    testing scopeUri
    Signed-off-by: sowmyahsg <sowmyahsg@gmail.com>

--- examples/logical_enclosure.go
+++ examples/logical_enclosure.go
@@ -10,10 +10,11 @@ import (
 func main() {
 	var (
 		ClientOV            *ov.OVClient
-		logical_enclosure   = "log_enc_66"
-		logical_enclosure_1 = "log_enclosure77"
-		logical_enclosure_2 = "log_enclosure88"
-		logical_enclosure_3 = "log_enclosure88"
+		logical_enclosure   = "sam" //"log_enc_66"
+//		logical_enclosure_1 = "log_enclosure77"
+//		logical_enclosure_2 = "log_enclosure88"
+//		logical_enclosure_3 = "log_enclosure88"
+		scope_name = "new-SD5"
 	)
 	ovc := ClientOV.NewOVClient(
 		os.Getenv("ONEVIEW_OV_USER"),
@@ -32,8 +33,11 @@ func main() {
 		fmt.Println(log_en)
 	}
 
+	scope1, err := ovc.GetScopeByName(scope_name)
+	fmt.Println(scope1)
+	fmt.Println(scope1.Uri)
 	sort := "name:desc"
-	log_en_list, err := ovc.GetLogicalEnclosures("", "", "", "", sort)
+	log_en_list, err := ovc.GetLogicalEnclosures("", "", "", utils.NewNstring(scope1.Uri), sort)
 	if err != nil {
 		fmt.Println(err)
 	} else {
@@ -42,7 +46,7 @@ func main() {
 			fmt.Println(log_en_list.Members[i].Name)
 		}
 	}
-
+/*
 	enclosureUris := new([]utils.Nstring)
 
 	*enclosureUris = append(*enclosureUris, utils.NewNstring("/rest/enclosures/0000000000A66101"))
@@ -83,5 +87,5 @@ func main() {
 	} else {
 		fmt.Println("#...................... Deleted Logical Enclosure Successfully .....#")
 	}
-
+*/
 }
