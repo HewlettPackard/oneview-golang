301347b015e922dea686a6dfdb0d5f5c879b54c6
Jyothis G M <jyothis@hpe.com>
2019-02-19 15:45:26 +0530

    Server Hardware firmware endpoint.

--- examples/server_hardware.go
+++ examples/server_hardware.go
@@ -20,7 +20,7 @@ func main() {
 		"*")
 
 	fmt.Println("Get server hardware list by name")
-	serverName, err := ovc.GetServerHardwareByName("0000A66101, bay 4")
+	serverName, err := ovc.GetServerHardwareByName("EN1, bay 1")
 	if err != nil {
 		fmt.Println("Failed to fetch server hardware name: ", err)
 	} else {
@@ -35,7 +35,7 @@ func main() {
 
 	fmt.Println("******************")
 
-	ServerId, err := ovc.GetServerHardwareByUri("/rest/server-hardware/30373737-3237-4D32-3230-313530314752")
+	ServerId, err := ovc.GetServerHardwareByUri("/rest/server-hardware/36343537-3338-4E43-3736-30333036524D")
 	if err == nil {
 		fmt.Println(ServerId.URI)
 	} else {
@@ -45,7 +45,7 @@ func main() {
 	fmt.Println("Get server-hardware list statistics specifying parameters")
 	fmt.Println("******************")
 
-	filters := []string{"name matches '0000A66101, bay 3'"}
+	filters := []string{"name matches 'EN1, bay 1'"}
 	ServerList, err := ovc.GetServerHardwareList(filters, "name:ascending")
 	if err == nil {
 		fmt.Println("Total server list :", ServerList.Total)
@@ -53,6 +53,17 @@ func main() {
 		fmt.Println("Failed to fetch server List : ", err)
 	}
 	fmt.Println("Get server-hardware whose profiles are unassigned ")
+
+	fmt.Println("Get firmware inventory for a server-hardware")
+	fmt.Println("******************")
+	firmware, err := ovc.GetServerFirmwareByUri(ServerId.URI)
+
+	if err == nil {
+		fmt.Println(firmware.ServerName)
+	} else {
+		fmt.Println("Failed to fetch Firmwares: ", err)
+	}
+
 	fmt.Println("******************")
 	serverHarware, err := ovc.GetAvailableHardware("/rest/server-hardware-types/9F529AA5-2021-4A10-93ED-DDC5BD80E949", "/rest/enclosure-groups/293e8efe-c6b1-4783-bf88-2d35a8e49071")
 
