9ceaa3b648253805d2ed784328886e8446ba137a
AnushaY1916 <anuanusha471@gmail.com>
2019-06-27 15:18:44 +0530

    Gofmt issue
    Signed-off-by: AnushaY1916 <anuanusha471@gmail.com>

--- examples/li.go
+++ examples/li.go
@@ -1,39 +1,42 @@
-
 package main
 
 import (
-        "fmt"
-        "github.com/HewlettPackard/oneview-golang/ov"
-        "os"
+	"fmt"
+	"github.com/HewlettPackard/oneview-golang/ov"
+	"os"
 )
 
 func main() {
-        var (
-                clientOV        *ov.OVClient
-                id              = "fe7dec60-896a-4418-9b3d-24ae9ba6fa6f"
+	var (
+		clientOV *ov.OVClient
+		id       = "d4468f89-4442-4324-9c01-624c7382db2d"
 	)
 
 	ovc := clientOV.NewOVClient(
-                os.Getenv("ONEVIEW_OV_USER"),
-                os.Getenv("ONEVIEW_OV_PASSWORD"),
-                os.Getenv("ONEVIEW_OV_DOMAIN"),
-                os.Getenv("ONEVIEW_OV_ENDPOINT"),
-                false,
-                800,
-                "*")
+		os.Getenv("ONEVIEW_OV_USER"),
+		"P@ssw0rd!",
+		os.Getenv("ONEVIEW_OV_DOMAIN"),
+		os.Getenv("ONEVIEW_OV_ENDPOINT"),
+		false,
+		800,
+		"*")
 
 	fmt.Println("....  Logical Interconnects Collection .....")
-        logicalInterconnectList, _ := ovc.GetLogicalInterconnects("", "0", "10")
-        fmt.Println(logicalInterconnectList)
+	fmt.Println(ovc.GetAPIVersion())
+	logicalInterconnectList, _ := ovc.GetLogicalInterconnects("", "", "")
+	//        fmt.Println(logicalInterconnectList)
+	for i := range logicalInterconnectList.Members {
+		fmt.Println(logicalInterconnectList.Members[i].Name)
+	}
 
-        fmt.Println("....  Logical Interconnect by Id.....")
-        lig, _ := ovc.GetLogicalInterconnectById(id)
+	fmt.Println("....  Logical Interconnect by Id.....")
+	lig, _ := ovc.GetLogicalInterconnectById(id)
 	fmt.Println("LI by ID")
-        fmt.Println(lig)
+	fmt.Println(lig.Name)
 
-	err_compliance := ovc.UpdateLogicalInterconnectConsistentStateById(id)
+	err_update_compliance := ovc.UpdateLogicalInterconnectConsistentStateById(id)
 
-	if err_compliance != nil {
-                fmt.Println("Could not update ConsistentState of Logical Interconnect", err_compliance)
+	if err_update_compliance != nil {
+		fmt.Println("Could not update ConsistentState of Logical Interconnect", err_update_compliance)
 	}
 }
