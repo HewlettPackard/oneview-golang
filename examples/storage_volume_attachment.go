e169321807ec8313ca2e9a4e49380c1354027011
Nabhajit Ray <nabhajit.ray@hpe.com>
2020-07-14 05:19:19 -0400

    API1800 support for VOlume attachment

--- examples/storage_volume_attachment.go
+++ examples/storage_volume_attachment.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/HewlettPackard/oneview-golang/ov"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -19,7 +20,7 @@ func main() {
 		os.Getenv("ONEVIEW_OV_DOMAIN"),
 		os.Getenv("ONEVIEW_OV_ENDPOINT"),
 		false,
-		1600,
+		1800,
 		"*")
 
 	// Get All the attachments present
@@ -32,7 +33,12 @@ func main() {
 	for i := 0; i < len(attachment_list.Members); i++ {
 		fmt.Println(attachment_list.Members[i].URI)
 	}
+	id := filepath.Base(string(attachment_list.Members[0].URI))
 
+	// Get volume attachment by id
+	fmt.Println("\nGetting details of volume attachment with name: ", id)
+	volAttach_by_id, _ := ovc.GetStorageAttachmentById(id)
+	fmt.Println(volAttach_by_id)
 	// Get volume attachment by name
 	fmt.Println("\nGetting details of volume attachment with name: ", name_to_get)
 	volAttach_by_name, _ := ovc.GetStorageAttachmentByName(name_to_get)
