43986509a6a2b87cd72a1ec451c6404ea9996117
Shanmugam <m-.shanmugam@hpe.com>
2021-05-26 17:42:52 +0530

    added_snmptrap

---
+++ examples/appliance_snmp_v3_trap_destinations.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/HewlettPackard/oneview-golang/ov"
+	"github.com/HewlettPackard/oneview-golang/utils"
+)
+
+func main() {
+	var (
+		ClientOV *ov.OVClient
+	)
+	apiversion, _ := strconv.Atoi(os.Getenv("ONEVIEW_APIVERSION"))
+	ovc := ClientOV.NewOVClient(
+		os.Getenv("ONEVIEW_OV_USER"),
+		os.Getenv("ONEVIEW_OV_PASSWORD"),
+		os.Getenv("ONEVIEW_OV_DOMAIN"),
+		os.Getenv("ONEVIEW_OV_ENDPOINT"),
+		false,
+		apiversion,
+		"*")
+
+	snmpUser := ov.SNMPv3Trap{
+		DestinationAddress: "1.1.1.1",
+		Port:               162,
+		UserID:             "d1c813f0-04a9-49e2-be31-01541d3a0912",
+	}
+
+	//Creating an SNMPv3 Trap Destinations
+	err := ovc.CreateSNMPv3TrapDestinations(snmpUser)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("#-----Created SNMPv3 Trap Destinations-----#")
+	}
+
+	//Get all the SNMPv3 Trap Destinations
+	allTraps, err := ovc.GetSNMPv3TrapDestinations("", "", "", "")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("#-----Got All SNMPv3 Trap Destinations-----#")
+		for i := range allTraps.Members {
+			fmt.Println(allTraps.Members[i])
+		}
+	}
+
+	//Get SNMPv3 Trap Destinations by ID
+	id := allTraps.Members[0].ID
+	trapId, err := ovc.GetSNMPv3TrapDestinationsById(id)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("#-----Got SNMPv3 Trap Destinations of ID %v", id)
+		fmt.Println(trapId)
+	}
+
+	existingDestinations := new([]utils.Nstring)
+	*existingDestinations = append(*existingDestinations, utils.Nstring("1.1.1.1"))
+	snmpUser2 := ov.ValidateSNMPv3Address{
+		DestinationAddress:   "2.3.2.3",
+		ExistingDestinations: *existingDestinations,
+	}
+	err = ovc.ValidateDestinationAddress(snmpUser2)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Successfully validated the Destination Address.")
+	}
+
+	//updating SNMPv3 Trap Destination
+	update := ov.SNMPv3Trap{
+		ID:                 trapId.ID,
+		UserID:             snmpUser.UserID,
+		Port:               190,
+		DestinationAddress: "1.1.1.1",
+	}
+	response, err := ovc.UpdateSNMPv3TrapDestinations(update)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("#-----Updated SNMPv3 Trap Destinations of ID %v", update.ID)
+	}
+
+	//Delete SNMPv3 Trap Destinations by ID
+	err = ovc.DeleteSNMPv3TrapDestinations(id)
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("#-----Deleted SNMPv3 Trap Destinations of ID %v", id)
+	}
+}
