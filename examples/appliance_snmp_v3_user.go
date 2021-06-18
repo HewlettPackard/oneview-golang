ec13c1fd83b8e9dd615ea04d947361af1eb42a32
Nabhajit Ray <nabhajit.ray@hpe.com>
2021-06-18 16:02:13 +0530

    added snmp v3 user example

--- examples/appliance_snmp_v3_user.go
+++ examples/appliance_snmp_v3_user.go
@@ -23,7 +23,16 @@ func main() {
 		"*")
 
 	snmpv3User := ov.SNMPv3User{
-		UserName:                 "user47",
+		UserName:                 "user0",
+		SecurityLevel:            "Authentication and privacy",
+		AuthenticationProtocol:   "SHA1",
+		AuthenticationPassphrase: "authPass",
+		PrivacyProtocol:          "AES-128",
+		PrivacyPassphrase:        "1234567812345678",
+	}
+
+	snmpv3User1 := ov.SNMPv3User{
+		UserName:                 "user1",
 		SecurityLevel:            "Authentication and privacy",
 		AuthenticationProtocol:   "SHA1",
 		AuthenticationPassphrase: "authPass",
@@ -39,73 +48,69 @@ func main() {
 		fmt.Println("#-----Created SNMPv3 User-----#")
 	}
 
+	//Creating second SNMP User
+	_, err0 := ovc.CreateSNMPv3Users(snmpv3User1)
+	if err0 != nil {
+		panic(err0)
+	} else {
+		fmt.Println("#-----Created SNMPv3 User-----#")
+	}
+
 	//Get all the SNMPv3 User
 	allsnmpv3user, err1 := ovc.GetSNMPv3Users("", "", "", "")
 	if err1 != nil {
 		panic(err1)
 	} else {
-		fmt.Println("#-----Got All SNMPv1 Trap Destinations-----#")
+		fmt.Println("#-----Got All SNMPv3 Users-----#")
 		for i := range allsnmpv3user.Members {
 			fmt.Println(allsnmpv3user.Members[i])
 		}
 	}
 
-	//Get SNMPv3 User by ID
-	id := "49a96a2a-a200-44df-b600-3a4c3d9f267c"
-	snmpv3User, err2 := ovc.GetSNMPv3UserById(id)
-	if err2 != nil {
-		panic(err2)
-	} else {
-		fmt.Printf("\n#-----Got SNMPv3 User of ID %v", id)
-		fmt.Println(snmpv3User)
-	}
-
 	//Get SNMPv3 User by username
-	username := "user47"
-	snmpv3User, err3 := ovc.GetSNMPv3UserByUserName(username)
+	snmpv3UserUserName, err3 := ovc.GetSNMPv3UserByUserName(snmpv3User.UserName)
 	if err3 != nil {
 		panic(err3)
 	} else {
-		fmt.Printf("\n#-----Got SNMPv3 User of UserName %v", username)
-		fmt.Println(snmpv3User)
+		fmt.Printf("\n#-----Got SNMPv3 User of UserName %v", snmpv3User.UserName)
+		fmt.Println(snmpv3UserUserName)
+	}
+
+	//Get SNMPv3 User by ID
+
+	snmpv3UserID, err2 := ovc.GetSNMPv3UserById(snmpv3UserUserName.Id)
+	if err2 != nil {
+		panic(err2)
+	} else {
+		fmt.Printf("\n#-----Got SNMPv3 User of ID %v", snmpv3UserUserName.Id)
+		fmt.Println(snmpv3UserID)
 	}
 
 	//updating SNMPv3 User
 
-	updated_snmpv3user := ov.SNMPv3User{
-		Type:                     "Users",
-		URI:                      "/rest/appliance/snmpv3-trap-forwarding/users/1e706f76-c585-4fc5-8a2b-86af678e4be1",
-		Category:                 "SnmpV3User",
-		ETAG:                     "2021-06-14T16:49:41.137Z",
-		Id:                       "1e706f76-c585-4fc5-8a2b-86af678e4be1",
-		UserName:                 "user2",
-		SecurityLevel:            "Authentication and privacy",
-		AuthenticationProtocol:   "SHA1",
-		AuthenticationPassphrase: "dsfdsfdsfdfdsfds",
-		PrivacyProtocol:          "AES-128",
-		PrivacyPassphrase:        "dsfdsfdsf545454",
-	}
-	response, err4 := ovc.UpdateSNMPv3User(updated_snmpv3user, id)
+	snmpv3UserID.PrivacyPassphrase = "55555555"
+	snmpv3UserID.AuthenticationPassphrase = "aaaaaaaaa"
+
+	response, err4 := ovc.UpdateSNMPv3User(snmpv3UserID, snmpv3UserUserName.Id)
 	if err4 != nil {
 		panic(err4)
 	} else {
-		fmt.Printf("\n#-----Updated SNMPv1 Trap Destinations port and community string: %v\n", response.URI)
+		fmt.Printf("\n#-----Updated SNMPv3 User Privacy passphrase and authenction passphrase : %v\n", response.URI)
 	}
 
 	// //Delete SNMPv3 User by ID
-	err5 := ovc.DeleteSNMPv3UserById(id)
+	err5 := ovc.DeleteSNMPv3UserById(response.Id)
 	if err5 != nil {
 		panic(err5)
 	} else {
-		fmt.Printf("\n#-----Deleted SNMPv3 User of ID %v\n", id)
+		fmt.Printf("\n#-----Deleted SNMPv3 User of ID %v\n", response.Id)
 	}
 	//Delete SNMPv3 User by UserName
-	username1 := "user3"
 
-	err6 := ovc.DeleteSNMPv3UserByName(username1)
+	err6 := ovc.DeleteSNMPv3UserByName(snmpv3User1.UserName)
 	if err5 != nil {
 		panic(err6)
 	} else {
-		fmt.Printf("\n#-----Deleted SNMPv3 User of Username %v\n", username1)
+		fmt.Printf("\n#-----Deleted SNMPv3 User of Username %v\n", snmpv3User1.UserName)
 	}
 }
