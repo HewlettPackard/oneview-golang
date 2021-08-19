3254550ccc8eb214e27274e9e9ce0931a602da6e
Nabhajit Ray <nabhajit.ray@hpe.com>
2021-08-19 15:53:15 +0530

    Added inital scope uri in le and sserver profile description in profile

--- ov/profiles.go
+++ ov/profiles.go
@@ -492,6 +492,7 @@ func (c *OVClient) CreateProfileFromTemplate(name string, template ServerProfile
 		new_template.Type = "ServerProfileV12"
 	}
 	new_template.ServerProfileTemplateURI = template.URI // create relationship
+	new_template.Description = template.ServerProfileDescription
 	new_template.ConnectionSettings = ConnectionSettings{
 		Connections: template.ConnectionSettings.Connections,
 	}
