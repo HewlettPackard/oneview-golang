2184ec9fb082c9cfbe43d2e837d30722ec3ac8a6
nabhajit-ray <nabhajit.ray@hpe.com>
2022-11-07 12:35:52 +0530

    fixing comments

--- examples/rack_managers.go
+++ examples/rack_managers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"path"
 
 	"github.com/HewlettPackard/oneview-golang/ov"
 )
@@ -9,9 +10,7 @@ import (
 func main() {
 	var (
 		ClientOV        *ov.OVClient
-		RackManagerName = "5UF7201002" //"TestRackManagerGOsdk"
-		// new_fc_name = "RenamedFCNetwork"
-		// falseVar    = false
+		RackManagerName = "TestRackManagerGOsdk"
 	)
 	config, config_err := ov.LoadConfigFile("config.json")
 	if config_err != nil {
@@ -25,10 +24,6 @@ func main() {
 		config.OVCred.SslVerify,
 		config.OVCred.ApiVersion,
 		config.OVCred.IfMatch)
-
-	// scope := ov.Scope{Name: "ScopTest", Description: "Test from script", Type: "ScopeV3"}
-	// _ = ovc.CreateScope(scope)
-	// scp, _ := ovc.GetScopeByName("ScopTest")
 
 	sort := "name:desc"
 
@@ -50,20 +45,19 @@ func main() {
 	}
 
 	rackManager := ov.RackManager{
-		Hostname: "172.18.9.3",
-		UserName: "dcs",
-		Password: "dcs",
-
-		//InitialScopeUris: *initialScopeUris,
+		Hostname: "<rack-manager-hostname>",
+		UserName: "<rack-manager-username>",
+		Password: "<rack-manager-password>",
 	}
 
-	rmId, err := ovc.AddRackManager(rackManager)
+	rmUri, err := ovc.AddRackManager(rackManager)
+	rmID := path.Base(rmUri)
 	if err != nil {
 		fmt.Println("............... Add Rack Manager Failed:", err)
 	} else {
-		fmt.Println(".... Create Rack Manager Success", rmId)
+		fmt.Println(".... Create Rack Manager Success", rmID)
 	}
-	rmCreated, err := ovc.GetRackManagerById(rmId)
+	rmCreated, err := ovc.GetRackManagerById(rmID)
 	fmt.Println(rmCreated.Name)
 	rmcName := rmCreated.Name
 
@@ -73,39 +67,5 @@ func main() {
 	} else {
 		fmt.Println("Deleted Rack manager successfully...")
 	}
-
-	// fcNetwork01 := ov.FCNetwork{
-	// 	AutoLoginRedistribution: falseVar,
-	// 	Description:             "Test FC Network 1",
-	// 	LinkStabilityTime:       30,
-	// 	FabricType:              "FabricAttach",
-	// 	Name:                    "testName1",
-	// 	Type:                    "fc-networkV4",
-	// }
-	// err = ovc.CreateFCNetwork(fcNetwork01)
-
-	// fcNetwork02 := ov.FCNetwork{
-	// 	AutoLoginRedistribution: falseVar,
-	// 	Description:             "Test FC Network 2",
-	// 	LinkStabilityTime:       30,
-	// 	FabricType:              "FabricAttach",
-	// 	Name:                    "testName2",
-	// 	Type:                    "fc-networkV4",
-	// }
-	// err = ovc.CreateFCNetwork(fcNetwork02)
-
-	// fcNetwork1, err := ovc.GetFCNetworkByName("testName1")
-	// fcNetwork2, err = ovc.GetFCNetworkByName("testName2")
-
-	// network_uris := &[]utils.Nstring{fcNetwork1.URI, fcNetwork2.URI}
-	// bulkDeleteFCNetwork := ov.FCNetworkBulkDelete{FCNetworkUris: *network_uris}
-	// err = ovc.DeleteScope("ScopTest")
-	// err = ovc.DeleteBulkFcNetwork(bulkDeleteFCNetwork)
-
-	// if err != nil {
-	// 	fmt.Println("............. FC Network Bulk-Deletion Failed:", err)
-	// } else {
-	// 	fmt.Println(".... FC Network Bulk-Delete is Successful")
-	// }
 
 }
